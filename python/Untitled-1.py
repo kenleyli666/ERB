9ac8b73975f1e278e9695c26681da0c6335b2d98
kenleyli666 <107999510+kenleyli666@users.noreply.github.com>
2024-09-13 14:55:11 +0800

    Add files via upload

---
+++ python/Untitled-1.py
@@ -0,0 +1,13 @@
+score = int(input("請輸入你嘅分數（0-100）："))
+
+if score >= 90:
+    grade = 'A'
+elif score >= 80:
+    grade = 'B'
+elif score >= 70:
+    grade = 'C'
+elif score >= 60:
+    grade = 'D'
+else:
+    grade = 'F'
+print(f"你嘅分數係{score}，等級係{grade}。")
---
+++ python/Untitled-2.py
@@ -0,0 +1,5 @@
+total = 0
+for i in range(5):
+    n = int(input(f"請輸入第{i+1}個數字："))
+    total += n
+print(f"5個數字嘅總和是：{total:}")
